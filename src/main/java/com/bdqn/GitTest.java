301a6aaea59ab8d3756d9c0488f9067e58897232
superV <3384186945@qq.com>
2022-07-11 16:31:17 +0800

    first commit

---
+++ src/main/java/com/bdqn/GitTest.java
@@ -0,0 +1,9 @@
+package com.bdqn;
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        System.out.println("Hello");
+        System.out.println("Hello");
+    }
+}
